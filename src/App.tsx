f035ff0abeb3b86503c397d0a6826a16a59bfc7e
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-17 13:40:58 +0700

    Add Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
+import { Caraousel } from "./components/Carousel";
 
 export const App = () => (
   <ChakraProvider>
     <NavBar />
+    <Caraousel />
   </ChakraProvider>
 );
