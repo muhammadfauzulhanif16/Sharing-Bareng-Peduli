9eeaed8995f67022fe57e2557d306b97b6c5a628
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-18 00:47:24 +0700

    Be a short directory for components

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export { About } from "./About";
+export { Carousel } from "./Carousel/Carousel";
+export { CTA } from "./CTA";
+export { Footer } from "./Footer";
+export { Member } from "./Member/Member";
+export { NavBar } from "./NavBar";
+export { Program } from "./Program";
