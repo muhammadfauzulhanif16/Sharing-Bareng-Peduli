9e19412d8e204fa280b1ad125808a7d3c0778d86
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-17 13:41:31 +0700

    Add About

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 import { Caraousel } from "./components/Carousel";
+import { About } from "./components/About";
 
 export const App = () => (
   <ChakraProvider>
     <NavBar />
     <Caraousel />
+    <About />
   </ChakraProvider>
 );
