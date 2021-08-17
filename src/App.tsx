7f3573dee71d6fd4b6acea3b3b375aa4876a6e9b
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-17 13:41:58 +0700

    Add Member

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 import { Caraousel } from "./components/Carousel";
 import { About } from "./components/About";
+import { Member } from "./components/Member";
 
 export const App = () => (
   <ChakraProvider>
     <NavBar />
     <Caraousel />
     <About />
+    <Member />
   </ChakraProvider>
 );
