b1d589c6ca0e188c18801ebe61574ece1d85195b
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-18 00:50:58 +0700

    Create member list clean code

---
+++ src/components/Member/MemberList.tsx
@@ -0,0 +1,55 @@
+import AMB from "../../img/member/AhnafMushyafaBisri.jpg";
+import DR from "../../img/member/DiajengRetnowati.jpg";
+import FAS from "../../img/member/FandhyArdhanaSaputra.jpg";
+import KK from "../../img/member/KhusnulKhotimah.jpg";
+import MFH from "../../img/member/MuhammadFauzulHanif.jpg";
+import NN from "../../img/member/NanaSuryana.jpg";
+import NSPP from "../../img/member/NilaSariPuspitaPutri.jpg";
+
+export const MemberList = [
+  {
+    name: "Nana Suryana",
+    img: `${NN}`,
+    occupation: "Founder",
+  },
+  {
+    name: "Ahnaf Mushyafa Bisri",
+    img: `${AMB}`,
+    occupation: "Co-Founder & Financial Manager",
+  },
+  {
+    name: "Nila Sari Puspita Putri",
+    img: `${NSPP}`,
+    occupation: "Financial Manager",
+  },
+  {
+    name: "Diajeng Retnowati",
+    img: `${DR}`,
+    occupation: "Public Relations",
+  },
+  {
+    name: "Khusnul Khotimah",
+    img: `${KK}`,
+    occupation: "Public Relations",
+  },
+  {
+    name: "Faridah Septiani",
+    img: ``,
+    occupation: "Content Writer",
+  },
+  {
+    name: "Muhammad Raihan Firdaus",
+    img: ``,
+    occupation: "Social Media Designer",
+  },
+  {
+    name: "Fandhy Ardhana Saputra",
+    img: `${FAS}`,
+    occupation: "Visuals & Video Editor",
+  },
+  {
+    name: "Muhammad Fauzul Hanif",
+    img: `${MFH}`,
+    occupation: "Web Developer",
+  },
+];
