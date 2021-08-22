2645645fc87d7baa646c36aab381638aedef9562
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-22 12:06:23 +0700

    change to be HeroList

--- src/components/Hero/HeroList.tsx
+++ src/components/Hero/HeroList.tsx
@@ -1,4 +1,4 @@
-export const CarouselList = [
+export const HeroList = [
   {
     img: "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     label: "First Slide",
