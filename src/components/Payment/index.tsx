10a4fb681854765f06319def70b395e5041cfbba
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-22 12:04:18 +0700

    change to be images folder

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -1,15 +1,16 @@
-import { Box, Container, Heading, Image, Grid, Stack } from "@chakra-ui/react";
-import ATM from "../img/payment/ATM_Bersama.png";
-import BCA from "../img/payment/BCA.png";
-import BNI from "../img/payment/BNI.png";
-import Dana from "../img/payment/Dana.png";
-import Gopay from "../img/payment/Gopay.png";
-import LinkAja from "../img/payment/LinkAja.png";
-import Ovo from "../img/payment/Ovo.png";
+import { Box, Container, Grid, Heading, Image, Stack } from "@chakra-ui/react";
+
+import ATMBersama from "../../images/payment/ATMBersama.png";
+import BCA from "../../images/payment/BCA.png";
+import BNI from "../../images/payment/BNI.png";
+import Dana from "../../images/payment/Dana.png";
+import Gopay from "../../images/payment/Gopay.png";
+import LinkAja from "../../images/payment/LinkAja.png";
+import Ovo from "../../images/payment/Ovo.png";
 
 export const Payment = () => {
   const payments = [
-    { name: "ATM Bersama", img: `${ATM}` },
+    { name: "ATM Bersama", img: `${ATMBersama}` },
     { name: "BCA", img: `${BCA}` },
     { name: "BNI", img: `${BNI}` },
     { name: "Dana", img: `${Dana}` },
@@ -18,7 +19,7 @@ export const Payment = () => {
     { name: "Ovo", img: `${Ovo}` },
   ];
   return (
-    <Box p={4}>
+    <Box p={10}>
       <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
         <Heading fontSize={"3xl"}>Mitra Pembayaran</Heading>
       </Stack>
