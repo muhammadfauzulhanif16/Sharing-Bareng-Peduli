e272a64487680fdb7a755437f1a6da83cf9734c8
Muhammad Fauzul Hanif <fauzul@crowde.co>
2021-08-28 12:07:42 +0700

    delete files

--- src/components/NavBar/NavList.tsx
+++
@@ -1,7 +0,0 @@
-export const NavList = [
-  { name: "Home", id: "#" },
-  { name: "About", id: "#about" },
-  { name: "Member", id: "#member" },
-  { name: "Program", id: "#program" },
-  { name: "Contact", id: "#contact" },
-];
